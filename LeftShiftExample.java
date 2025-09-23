12636433eb1087a9063e608d93011ff900d60d5a
Raagavarshini <kurmavarshini07@gmail.com>
2025-09-23 20:01:33 +0530

    Add files via upload

---
+++ LeftShiftExample.java
@@ -0,0 +1,6 @@
+public class LeftShiftExample {
+public static void main (String[] args) {
+int a = 5; // 0101
+int result = a << 2; // 0101 → 10100 = 20 System.out.print/n("a << 2="+ result);
+}
+}
