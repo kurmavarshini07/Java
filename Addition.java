59e276837422d604c8277815b95e8dcd86c3b475
Raagavarshini <kurmavarshini07@gmail.com>
2025-09-23 19:23:50 +0530

    Add files via upload

---
+++ Addition.java
@@ -0,0 +1,7 @@
+public class Addition{
+    public static void main (String[] args){
+        int num1=10,num2=20;
+        int sum=num1+num2;
+        System.out.println("Addition of two num"+sum);
+    }
+}
