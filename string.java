2ed3154ac3c8970592c5bbda87dd94727066b779
Raagavarshini <kurmavarshini07@gmail.com>
2025-09-23 19:58:31 +0530

    Add files via upload

---
+++ string.java
@@ -0,0 +1,18 @@
+public class string {
+    public static void main(String[] args) {
+        String a = "Hello";
+        String b = "hello";
+        System.out.println(a);
+        System.out.println(a.toUpperCase());
+        System.out.println(a.toLowerCase());
+        System.out.println(a.length());
+        System.out.println(a.charAt(0));
+        if(a.equalsIgnoreCase(b)){
+            System.out.println("Both the strings are equal");
+        }
+        else{
+            System.out.println("They are not equal");
+        }
+        
+    }
+}
